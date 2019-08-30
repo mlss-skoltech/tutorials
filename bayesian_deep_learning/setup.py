726963f6319bf0be77d67862d00d350e76b01b94
ivannz <ivan.nazarov@skolkovotech.ru>
2019-08-30 08:14:31 +0300

    updated setup with subpackage

--- bayesian_deep_learning/setup.py
+++ bayesian_deep_learning/setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 
 setup(
     name="mlss2019bdl",
-    version="0.1",
+    version="0.2",
     description="""Service code for MLSS2019 Tutorial on Bayesian Deep Learning""",
     license="MIT License",
     author="Ivan Nazarov, Yarin Gal",
     author_email="ivan.nazarov@skolkovotech.ru",
     packages=[
         "mlss2019bdl",
+        "mlss2019bdl.bdl",
     ],
     install_requires=[
         "numpy",
